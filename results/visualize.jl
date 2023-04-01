262dce143fcadbc44c157ad6b289321037b928b9
nticea <nticea@stanford.edu>
2023-04-01 17:19:15 -0400

    repeating 29x29 calculation

---
+++ results/visualize.jl
@@ -0,0 +1,36 @@
+## IMPORTS ##
+using Pkg
+Pkg.activate(joinpath(@__DIR__, ".."))
+include("../src/model.jl")
+using Plots
+using CSV
+using DataFrames
+using StatsPlots
+
+L = 49
+files = readdir(@__DIR__)
+
+# nodenames = ["L", "J", "V0", "T", "λ"]
+# df = DataFrame([name => [] for name in nodenames])
+# for f in files
+#     if startswith(f, "$(L)Nx$(L)Ny")
+#         dfi = DataFrame(CSV.File(joinpath(@__DIR__, f)))
+#         append!(df, dfi)
+#     end
+# end
+loadpath = "/Users/nicole/Dropbox/Grad school/Trithep/quasiperiodic/QuasiperiodicSuperconductivity/results/39Nx39Ny_results.csv"
+df = DataFrame(CSV.File(loadpath))
+
+Tc_df = find_Tc(df)
+
+# Now, do plotting 
+@df Tc_df plot(
+    :V0,
+    :Tc,
+    group=:J,
+    m=(0.75, [:+ :h :star7 :circle], 5),
+    xaxis=:log10, yaxis=:log10,
+)
+title!("Transition temperature")
+xlabel!("V")
+ylabel!("Tc")
--- src/results.jl
+++ src/results.jl
@@ -83,6 +83,46 @@ function find_Tc(results::Results)
     return Tcs
 end
 
+function find_Tc(df::DataFrame)
+
+    # Get the unique Js, V0s from the dataframe 
+    Js, V0s = unique(df.J), unique(df.V0)
+    L = df.L[1]
+
+    # Make the new dataframe 
+    nodenames = ["L", "J", "V0", "Tc"]
+    Tc_df = DataFrame([name => [] for name in nodenames])
+
+    # For each unique (J,V0) pair... extract the corresponding data across all Ts 
+    for J in Js
+        for V0 in V0s
+
+            # Extract the corresponding data across all Ts
+            dfsub = df[(df.J.==J).&(df.V0.==V0), :]
+            λs, Ts = dfsub.λ, dfsub.T
+
+            # Compute the interpolated Tc for this (J,V0) pair
+            knots = reverse(λs)
+            if length(knots) > 1
+                Interpolations.deduplicate_knots!(knots, move_knots=true)
+                try
+                    interp_linear = linear_interpolation(knots, reverse(Ts))
+                    Tc = interp_linear(1)
+                    # Put it into a new dataframe indexed by (J,V0,Tc)
+                    df2 = DataFrame(L=[L], Tc=[Tc], J=[J], V0=[V0])
+                    append!(Tc_df, df2)
+                catch e
+                    Tc = NaN
+                    # Put it into a new dataframe indexed by (J,V0,Tc)
+                    df2 = DataFrame(L=[L], Tc=[Tc], J=[J], V0=[V0])
+                    append!(Tc_df, df2)
+                end
+            end
+        end
+    end
+    return Tc_df
+end
+
 function plot_Tcs(results::Results)
     L, λs, Js, V0s, Ts = results.L, results.λs, results.Js, results.V0s, results.Ts
     Tcs = find_Tc(results)
