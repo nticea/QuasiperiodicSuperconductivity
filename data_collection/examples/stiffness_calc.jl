8de5273d3d56553a2e4e9a4ba3e9a9e9bb5d4178
nticea <nticea@stanford.edu>
2023-06-19 21:01:08 -0700

    initial stiffness calculations

--- data_collection/examples/stiffness_calc.jl
+++ data_collection/examples/stiffness_calc.jl
@@ -10,21 +10,23 @@ using DataFrames
 include("../../src/stiffness.jl")
 
 ## PARAMETERS ##
-L = 9 # the full system is L × L 
+L = 11 # the full system is L × L 
 t = 1 # hopping 
 Q = (√5 - 1) / 2
 μ = 1e-8
-θ = nothing#π / 7
-ϕx = 0#0.375
-ϕy = 0#0.29
+θ = π / 7
+ϕx = 0.375
+ϕy = 0.29
 V0 = -1
 V1 = 0
 J = 2
 periodic = true
 niter = 500
-tol = 1e-6
-pairing_symmetry = "d-wave"
-noise = 0#1e-3 # looks like adding noise is not helping anything 
+tol = 1e-12
+noise = 0
 
-T = 0.07
-superfluid_stiffness(T; L=L, t=t, J=J, Q=Q, θ=θ, ϕx=ϕx, ϕy=ϕy, μ=μ, V0=V0, V1=V1, periodic=periodic, niter=niter, tol=tol, noise=noise)
+T = 0.11
+λ, Δ = pairfield_correlation(T, L=L, t=t, J=J, Q=Q, θ=θ, ϕx=ϕx, ϕy=ϕy, μ=μ, V0=V0, V1=V1, periodic=periodic, symmetry="d-wave")
+K, Π = superfluid_stiffness(T, L=L, t=t, J=J, Q=Q, μ=μ, V0=V0, V1=V1, tol=tol, θ=θ, ϕx=ϕx, ϕy=ϕy, niter=niter, periodic=periodic, noise=noise)
+
+
