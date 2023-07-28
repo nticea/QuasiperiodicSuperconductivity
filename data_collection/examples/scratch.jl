65a730159aff2797d488290f19a8acbbf6c06376
nticea <nticea@stanford.edu>
2023-07-28 15:19:37 -0700

    deleting some old results

--- data_collection/examples/scratch.jl
+++ data_collection/examples/scratch.jl
@@ -12,7 +12,7 @@ include("../../src/stiffness.jl")
 include("../../src/meanfield.jl")
 
 ## PARAMETERS ##
-L = 17 # the full system is L × L 
+L = 11 # the full system is L × L 
 t = 1 # hopping 
 Q = (√5 - 1) / 2
 μ = 1e-8
@@ -26,55 +26,3 @@ LGE_tol = 1e-2
 BdG_tol = 1e-12
 niter = 500
 
-## Tc using LGE ##
-
-## Superfluid stiffness calculation ##
-T = 0.31 # everything is at 0 temperature
-Js = collect(0:0.5:4)
-cmap = cgrad(:matter, length(Js), categorical=true)
-p = plot()
-ptime = plot()
-pmem = plot()
-for (i, J) in enumerate(Js)
-    Λs = T * collect(1:1:15)
-    λs = []
-    times = []
-    mems = []
-    for Λ in Λs
-        res = @timed pairfield_correlation(T; L=L, t=t, J=J, Q=Q, θ=θ, ϕx=ϕx, ϕy=ϕy, μ=μ, V0=V0, V1=V1, periodic=periodic, Λ=Λ)
-        λ, Δ_LGE = res.value
-        push!(λs, λ)
-        push!(times, res.time)
-        push!(mems, res.bytes)
-    end
-    res = @timed pairfield_correlation(T; L=L, t=t, J=J, Q=Q, θ=θ, ϕx=ϕx, ϕy=ϕy, μ=μ, V0=V0, V1=V1, periodic=periodic)
-    λ0, _ = res.value
-    t0, m0 = res.time, res.bytes
-
-    plot!(p, Λs, λs, color=cmap[i], label=nothing)
-    scatter!(p, Λs, λs, color=cmap[i], label="J=$J")
-    hline!(p, [λ0], color=cmap[i], ls=:dashdot, label=nothing)
-
-    plot!(ptime, Λs, times, color=cmap[i], label=nothing)
-    scatter!(ptime, Λs, times, color=cmap[i], label=nothing)
-    hline!(ptime, [t0], color=cmap[i], ls=:dashdot, label=nothing)
-
-    plot!(pmem, Λs, mems, color=cmap[i], label=nothing)
-    scatter!(pmem, Λs, mems, color=cmap[i], label=nothing)
-    hline!(pmem, [m0], color=cmap[i], ls=:dashdot, label=nothing)
-end
-title!(p, "Maximum eigenvalue")
-xlabel!(p, "Λ")
-ylabel!(p, "λ")
-
-plot!(ptime, ylims=(0, 0.25))
-title!(ptime, "Runtime")
-xlabel!(ptime, "Λ")
-ylabel!(ptime, "Time (s)")
-
-title!(pmem, "Memory usage")
-xlabel!(pmem, "Λ")
-ylabel!(pmem, "Bytes")
-
-ps = plot(p, ptime, pmem, layout=Plots.grid(1, 3,
-        widths=[1 / 3, 1 / 3, 1 / 3]), size=(1500, 1000), plot_title="Keeping states within Λ of εF for V0=$V0, V1=$V1 on $L × $L lattice")
--- data_collection/examples/scratch2.jl
+++ data_collection/examples/scratch2.jl
@@ -12,6 +12,7 @@ include("../../src/stiffness.jl")
 include("../../src/meanfield.jl")
 
 ## PARAMETERS ##
+L = 55
 t = 1 # hopping 
 Q = (√5 - 1) / 2
 μ = 1e-8
@@ -23,36 +24,38 @@ J = 0
 Λ = 1
 
 Ts = expspace(-5, 1, 10)
-Ls = [17, 23, 31, 35]
-χs_dwave = zeros(length(Ls), length(Ts))
-χs_swave = zeros(length(Ls), length(Ts))
+Ls = [55]
+Λs = [1, nothing]
+χs_dwave = zeros(length(Λs), length(Ts))
+χs_swave = zeros(length(Λs), length(Ts))
 
+# χ = @time uniform_susceptibility(1e-2, "d-wave", L=67, t=t, J=J,
+#     Q=Q, θ=θ, ϕx=ϕx, ϕy=ϕy, μ=μ, periodic=periodic, Λ=1)
 
-χ = @time uniform_susceptibility(1e-2, "d-wave", L=55, t=t, J=J,
-    Q=Q, θ=θ, ϕx=ϕx, ϕy=ϕy, μ=μ, periodic=periodic, Λ=1)
-
-@assert 1 == 0
+# @assert 1 == 0
 
 for (i, T) in enumerate(Ts)
-    for (j, L) in enumerate(Ls)
-        χ = @time uniform_susceptibility(T, "d-wave", L=L, t=t, J=J,
+    for (j, Λ) in enumerate(Λs)
+        χ = @time uniform_susceptibility(T, "s-wave", L=L, t=t, J=J,
             Q=Q, θ=θ, ϕx=ϕx, ϕy=ϕy, μ=μ, periodic=periodic, Λ=Λ)
         # make the d-wave components 
-        xx = χ[1, 1]
-        yy = χ[2, 2]
-        xy = χ[1, 2]
-        yx = χ[2, 1]
+        # xx = χ[1, 1]
+        # yy = χ[2, 2]
+        # xy = χ[1, 2]
+        # yx = χ[2, 1]
 
-        χs_dwave[j, i] = xx + yy - xy - yx
-        χs_swave[j, i] = χ[3, 3]
+        # χs_dwave[j, i] = xx + yy - xy - yx
+        # χs_swave[j, i] = χ[3, 3]
+
+        χs_swave[j, i] = χ
     end
 end
 
 pswave = plot()
 c = palette(:tab10)
-for (j, L) in enumerate(Ls)
+for (j, Λ) in enumerate(Λs)
     plot!(pswave, Ts, χs_swave[j, :], c=c[j], label=nothing, xaxis=:log10)
-    scatter!(pswave, Ts, χs_swave[j, :], c=c[j], label="L=$L", xaxis=:log10)
+    scatter!(pswave, Ts, χs_swave[j, :], c=c[j], label="Λ=$Λ", xaxis=:log10)
 end
 title!("s-wave susceptibility")
 xlabel!("T")
