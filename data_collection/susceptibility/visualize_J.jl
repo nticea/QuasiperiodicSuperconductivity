5392d137ddff1ec9fa18d4b1ee3153dc0ed8eb03
nticea <nticea@stanford.edu>
2023-08-24 10:21:52 -0700

    visualizes

--- data_collection/susceptibility/visualize_J.jl
+++ data_collection/susceptibility/visualize_J.jl
@@ -12,11 +12,11 @@ include("utilities.jl")
 
 ## PARAMETERS ## 
 
-L = 35 # the full system is L × L 
+L = 23 # the full system is L × L 
 ndims = 3
 Q = (√5 - 1) / 2
 θ = π / 7
-savefigs = false
+savefigs = true
 figpath = mkpath(joinpath(@__DIR__, "figures"))
 T_cutoff = 1e-2
 
@@ -32,8 +32,6 @@ px, pos = plot(margin=10Plots.mm), plot(margin=10Plots.mm)
 dfL = df[(df.L.==L).&(df.θ.==θ).&(df.Q.==Q).&(df.ndims.==ndims), :]
 Js = sort(unique(dfL.J))
 cmap = cgrad(:matter, length(Js), categorical=true)
-
-@assert 1 == 0
 
 for (j, J) in enumerate(Js)
     dfJ = dfL[(dfL.J.==J), :]
@@ -75,17 +73,17 @@ for (j, J) in enumerate(Js)
         plot!(pos, Ts, χswave, color=cmap[j], label=nothing, xaxis=:log10, yaxis=:log10)
         scatter!(pos, Ts, χswave, color=cmap[j], label="J=$J, L=$L", xaxis=:log10, yaxis=:log10)
 
-        title!(px, "Susceptibility (d-wave)")
+        title!(px, "d-wave")
         xlabel!(px, "T")
         ylabel!(px, "χ")
-        title!(pos, "Susceptibility (s-wave)")
+        title!(pos, "s-wave")
         xlabel!(pos, "T")
         ylabel!(pos, "χ")
     end
 end
 
-p = plot(px, pos, layout=Plots.grid(1, 2,
-        widths=[1 / 2, 1 / 2]), size=(1700, 800), plot_title="Susceptibility")
+p1 = plot(px, pos, layout=Plots.grid(1, 2,
+        widths=[1 / 2, 1 / 2]), size=(1700, 800), plot_title="Susceptibility for $size_str lattice with Q=$(round(Q, digits=3)), θ=$(θ_to_π(θ))")
 if savefigs
     savefig(p, joinpath(figpath, "susceptibility_J_$(L)L.pdf"))
 end
@@ -106,6 +104,7 @@ for (i, T) in enumerate(Ts)
     sortidx = sortperm(Js)
     Js = Js[sortidx]
     χs = χs[sortidx]
+    χs = [reshape(χ, 4, 4) for χ in χs]
 
     # on-site
     χswave = [χ[1, 1] for χ in χs]
@@ -131,10 +130,10 @@ for (i, T) in enumerate(Ts)
     plot!(ptemp_d, Js, χdwave, label=nothing, c=cmap[i])
     scatter!(ptemp_d, Js, χdwave, c=cmap[i], label=nothing)
 
-    title!(ptemp_d, "Susceptibility (d-wave)")
+    title!(ptemp_d, "d-wave")
     xlabel!(ptemp_d, "J")
     ylabel!(ptemp_d, "χ")
-    title!(ptemp_s, "Susceptibility (s-wave)")
+    title!(ptemp_s, "s-wave")
     xlabel!(ptemp_s, "J")
     ylabel!(ptemp_s, "χ")
 end
@@ -142,8 +141,14 @@ end
 # make a colourbar 
 heatmap!(ptemp_s, zeros(2, 2), clims=(minimum(Ts), maximum(Ts)), cmap=:viridis, alpha=0)
 
+if ndims == 3
+    size_str = "$L × $L × $L"
+elseif ndims == 2
+    size_str = "$L × $L"
+end
+
 p2 = plot(ptemp_d, ptemp_s, layout=Plots.grid(1, 2,
-        widths=[1 / 2, 1 / 2]), size=(1700, 800), plot_title="Susceptibility")
+        widths=[1 / 2, 1 / 2]), size=(1700, 800), plot_title="Susceptibility for $size_str lattice with Q=$(round(Q, digits=3)), θ=$(θ_to_π(θ))")
 if savefigs
     savefig(p2, joinpath(figpath, "susceptibility_temp_$(L)L.pdf"))
 end
