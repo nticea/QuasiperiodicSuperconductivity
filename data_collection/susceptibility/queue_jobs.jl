2f7f9944fb794626477d4a110db642e925dc17e7
nticea <nticea@stanford.edu>
2023-09-19 15:01:37 -0700

    queue jobs

--- data_collection/susceptibility/queue_jobs.jl
+++ data_collection/susceptibility/queue_jobs.jl
@@ -20,18 +20,30 @@ ndims = 3
 # L = 27, time = 4 hrs, mem = 256
 # L = 23, time = 1.5 hrs, mem = 100
 
-Ls = [29]
 Js = collect(0:0.25:3)
 Ts = expspace(-3, 1, 30) # temperature 
 
 filepath = joinpath(@__DIR__, "collect_data.jl")
 job_prefix = "susceptibility"
 
-for L in Ls
-    for J in Js
-        for T in Ts
-            ps = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims)
-            submit_job(ps, filepath, @__DIR__, job_prefix, mem=350, kwargs="$T", time="10:00:00")
-        end
+# L = 23
+for J in Js
+    for T in Ts
+        ps = ModelParams(L=23, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims)
+        submit_job(ps, filepath, @__DIR__, job_prefix, mem=100, kwargs="$T", time="1:30:00")
+    end
+end
+
+for J in Js
+    for T in Ts
+        ps = ModelParams(L=27, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims)
+        submit_job(ps, filepath, @__DIR__, job_prefix, mem=255, kwargs="$T", time="6:00:00")
+    end
+end
+
+for J in Js
+    for T in Ts
+        ps = ModelParams(L=29, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims)
+        submit_job(ps, filepath, @__DIR__, job_prefix, mem=350, kwargs="$T", time="12:00:00")
     end
 end
