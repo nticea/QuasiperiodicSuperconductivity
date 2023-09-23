01e5776389858ae72f3c2adc300cb61533d2d40d
nticea <nticea@stanford.edu>
2023-09-23 11:54:41 -0700

    sync

--- data_collection/eigenval_susceptibility/visualize_J.jl
+++ data_collection/eigenval_susceptibility/visualize_J.jl
@@ -12,7 +12,7 @@ include("utilities.jl")
 
 ## PARAMETERS ## 
 
-L = 13 # the full system is L × L 
+L = 11 # the full system is L × L 
 ndims = 3
 Q = (√5 - 1) / 2
 θ = π / 7
@@ -26,18 +26,22 @@ if savefigs
 end
 df = load_dfs()
 
-px, pos = plot(margin=10Plots.mm), plot(margin=10Plots.mm)
-pxd, posd = plot(margin=10Plots.mm), plot(margin=10Plots.mm)
 # extract only the parameters we are interested in 
 dfL = df[(df.L.==L).&(df.θ.==θ).&(df.Q.==Q).&(df.ndims.==ndims), :]
+
+# use split-apply-combine
+gdf = groupby(dfL, [:J, :T])
+dfL = combine(gdf, [:χswave => mean, :χdwave => mean, :dχswave => mean, :dχdwave => mean])
+
+px, pos = plot(margin=10Plots.mm), plot(margin=10Plots.mm)
+pxd, posd = plot(margin=10Plots.mm), plot(margin=10Plots.mm)
 Js = sort(unique(dfL.J))
 cmap = cgrad(:matter, length(Js), categorical=true)
-
 for (j, J) in enumerate(Js)
     dfJ = dfL[(dfL.J.==J), :]
     Ts = dfJ.T
-    χswave = dfJ.χswave
-    χdwave = dfJ.χdwave
+    χswave = dfJ.χswave_mean
+    χdwave = dfJ.χdwave_mean
 
     sortidx = sortperm(Ts)
     Ts = Ts[sortidx]
@@ -88,8 +92,8 @@ ptemp_d = plot(margin=10Plots.mm)
 for (i, T) in enumerate(Ts)
     dfT = dfL[(dfL.T.==T), :]
     Js = dfT.J
-    χswave = dfT.χswave
-    χdwave = dfT.χdwave
+    χswave = dfT.χswave_mean
+    χdwave = dfT.χdwave_mean
     sortidx = sortperm(Js)
     Js = Js[sortidx]
     χswave = χswave[sortidx]
@@ -113,6 +117,95 @@ end
 heatmap!(ptemp_s, zeros(2, 2), clims=(minimum(Ts), maximum(Ts)), cmap=:viridis, alpha=0)
 
 p2 = plot(ptemp_d, ptemp_s, layout=Plots.grid(1, 2,
+        widths=[1 / 2, 1 / 2]), size=(1700, 800), plot_title="Eigenvalue χ for $size_str lattice with Q=$(round(Q, digits=3)), θ=$(θ_to_π(θ))")
+if savefigs
+    savefig(p2, joinpath(figpath, "susceptibility_temp_$(L)L.pdf"))
+end
+
+px, pos = plot(margin=10Plots.mm), plot(margin=10Plots.mm)
+pxd, posd = plot(margin=10Plots.mm), plot(margin=10Plots.mm)
+Js = sort(unique(dfL.J))
+cmap = cgrad(:matter, length(Js), categorical=true)
+for (j, J) in enumerate(Js)
+    dfJ = dfL[(dfL.J.==J), :]
+    Ts = dfJ.T
+    χswave = dfJ.dχswave_mean
+    χdwave = dfJ.dχdwave_mean
+
+    sortidx = sortperm(Ts)
+    Ts = Ts[sortidx]
+    χswave = χswave[sortidx]
+    χdwave = χdwave[sortidx]
+
+    # consider only the linear regime 
+    # χs = χs[Ts.>=T_cutoff]
+    # Ts = Ts[Ts.>=T_cutoff]
+
+    if length(Ts) > 0
+        plot!(px, Ts, χdwave, color=cmap[j], label=nothing, xaxis=:log10)
+        scatter!(px, Ts, χdwave, color=cmap[j], label="J=$J, L=$L", xaxis=:log10)
+
+        plot!(pos, Ts, χswave, color=cmap[j], label=nothing, xaxis=:log10)
+        scatter!(pos, Ts, χswave, color=cmap[j], label="J=$J, L=$L", xaxis=:log10)
+
+        title!(px, "d-wave")
+        xlabel!(px, "T")
+        ylabel!(px, "χ")
+        title!(pos, "s-wave")
+        xlabel!(pos, "T")
+        ylabel!(pos, "χ")
+    end
+end
+ylims!(px, (-1, 0))
+ylims!(pos, (-1, 0))
+
+
+if ndims == 3
+    size_str = "$L × $L × $L"
+elseif ndims == 2
+    size_str = "$L × $L"
+end
+p3 = plot(px, pos, layout=Plots.grid(1, 2,
+        widths=[1 / 2, 1 / 2]), size=(1700, 800), plot_title="Susceptibility for $size_str lattice with Q=$(round(Q, digits=3)), θ=$(θ_to_π(θ))")
+if savefigs
+    savefig(p1, joinpath(figpath, "susceptibility_J_$(L)L.pdf"))
+end
+
+# also, plot the value of each J at various temperatures 
+Ts = sort(unique(dfL.T))
+Ts = Ts[Ts.>=T_cutoff]
+cmap = cgrad(:viridis, length(Ts), categorical=true)
+
+ptemp_s = plot(margin=10Plots.mm)
+ptemp_d = plot(margin=10Plots.mm)
+for (i, T) in enumerate(Ts)
+    dfT = dfL[(dfL.T.==T), :]
+    Js = dfT.J
+    χswave = dfT.dχswave_mean
+    χdwave = dfT.dχdwave_mean
+    sortidx = sortperm(Js)
+    Js = Js[sortidx]
+    χswave = χswave[sortidx]
+    χdwave = χdwave[sortidx]
+
+    plot!(ptemp_s, Js, χswave, label=nothing, c=cmap[i])
+    scatter!(ptemp_s, Js, χswave, c=cmap[i], label=nothing)
+
+    plot!(ptemp_d, Js, χdwave, label=nothing, c=cmap[i])
+    scatter!(ptemp_d, Js, χdwave, c=cmap[i], label=nothing)
+
+    title!(ptemp_d, "d-wave")
+    xlabel!(ptemp_d, "J")
+    ylabel!(ptemp_d, "χ")
+    title!(ptemp_s, "s-wave")
+    xlabel!(ptemp_s, "J")
+    ylabel!(ptemp_s, "χ")
+end
+
+# make a colourbar 
+#heatmap!(ptemp_s, zeros(2, 2), clims=(minimum(Ts), maximum(Ts)), cmap=:viridis, alpha=0)
+
+p4 = plot(ptemp_d, ptemp_s, layout=Plots.grid(1, 2,
         widths=[1 / 2, 1 / 2]), size=(1700, 800), plot_title="Eigenvalue χ for $size_str lattice with Q=$(round(Q, digits=3)), θ=$(θ_to_π(θ))")
 if savefigs
     savefig(p2, joinpath(figpath, "susceptibility_temp_$(L)L.pdf"))
