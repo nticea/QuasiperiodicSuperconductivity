cbde9d3c8caaea48f53acd850980209e50fa0c91
nticea <nticea@stanford.edu>
2023-10-08 17:23:03 -0700

    more jobs disorder

--- data_collection/susceptibility/visualize_new.jl
+++ data_collection/susceptibility/visualize_new.jl
@@ -44,32 +44,38 @@ for g in gdf
     end
 end
 gdf = groupby(dfsummary, [:J, :T])
-dfmean = combine(gdf, [:χswave => mean, :χdwave => mean, :dχswave => mean, :dχdwave => mean])
+dfmean = combine(gdf, [:χswave => mean, :χdwave => mean, :dχswave => mean, :dχdwave => mean,
+    :χswave => sem, :χdwave => sem, :dχswave => sem, :dχdwave => sem])
 
 Js = sort(unique(dfmean.J))
-# cmap = cgrad(:matter, length(Js), categorical=true)
-cmap = ["red", "blue", "green", "orange"]
-pχswave, pχdwave, pdχswave, pdχdwave = plot(title="χ swave"), plot(title="χ dwave"), plot(title="dχdlogT swave"), plot(title="dχdlogT dwave")
+cmap = cgrad(:matter, length(Js), categorical=true)
+#cmap = ["red", "blue", "green", "orange"]
+pχswave, pχdwave, pdχswave, pdχdwave = plot(title="χ swave", grid=false), plot(title="χ dwave", grid=false), plot(title="dχdlogT swave", grid=false), plot(title="dχdlogT dwave", grid=false)
 for (Jᵢ, J) in enumerate(Js)
     dfJ = dfmean[(dfmean.J.==J), :]
     Ts, χswave, χdwave, dχswave, dχdwave = dfJ.T, dfJ.χswave_mean, dfJ.χdwave_mean, dfJ.dχswave_mean, dfJ.dχdwave_mean
+    σ_χswave, σ_χdwave, σ_dχswave, σ_dχdwave = dfJ.χswave_sem, dfJ.χdwave_sem, dfJ.dχswave_sem, dfJ.dχdwave_sem
     sortidx = sortperm(Ts)
     Ts = Ts[sortidx]
     χswave = χswave[sortidx]
     χdwave = χdwave[sortidx]
     dχswave = dχswave[sortidx]
     dχdwave = dχdwave[sortidx]
+    σ_χswave = σ_χswave[sortidx]
+    σ_χdwave = σ_χdwave[sortidx]
+    σ_dχswave = σ_dχswave[sortidx]
+    σ_dχdwave = σ_dχdwave[sortidx]
 
-    plot!(pχswave, Ts, χswave, xaxis=:log10, xlabel="T", ylabel="χ", label=nothing, c=cmap[Jᵢ])
+    plot!(pχswave, Ts, χswave, xaxis=:log10, xlabel="T", ylabel="χ", label=nothing, c=cmap[Jᵢ], ribbon=σ_χswave)
     scatter!(pχswave, Ts, χswave, xaxis=:log10, xlabel="T", ylabel="χ", label="J=$J", c=cmap[Jᵢ])
 
-    plot!(pχdwave, Ts, χdwave, xaxis=:log10, xlabel="T", ylabel="χ", label=nothing, c=cmap[Jᵢ])
+    plot!(pχdwave, Ts, χdwave, xaxis=:log10, xlabel="T", ylabel="χ", label=nothing, c=cmap[Jᵢ], ribbon=σ_χdwave)
     scatter!(pχdwave, Ts, χdwave, xaxis=:log10, xlabel="T", ylabel="χ", label="J=$J", c=cmap[Jᵢ])
 
-    plot!(pdχswave, Ts, dχswave, xaxis=:log10, xlabel="T", ylabel="dχ", label=nothing, c=cmap[Jᵢ])
+    plot!(pdχswave, Ts, dχswave, xaxis=:log10, xlabel="T", ylabel="dχ", label=nothing, c=cmap[Jᵢ], ribbon=σ_dχswave)
     scatter!(pdχswave, Ts, dχswave, xaxis=:log10, xlabel="T", ylabel="dχ", label="J=$J", c=cmap[Jᵢ])
 
-    plot!(pdχdwave, Ts, dχdwave, xaxis=:log10, xlabel="T", ylabel="dχ", label=nothing, c=cmap[Jᵢ])
+    plot!(pdχdwave, Ts, dχdwave, xaxis=:log10, xlabel="T", ylabel="dχ", label=nothing, c=cmap[Jᵢ], ribbon=σ_dχdwave)
     scatter!(pdχdwave, Ts, dχdwave, xaxis=:log10, xlabel="T", ylabel="dχ", label="J=$J", c=cmap[Jᵢ])
 
 end
