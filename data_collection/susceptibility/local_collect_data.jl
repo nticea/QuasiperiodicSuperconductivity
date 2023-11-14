6e438cad1e6c2186c1c5edee65b5ac0810038c6a
nticea <nticea@stanford.edu>
2023-11-14 13:14:28 -0800

    sync

--- data_collection/susceptibility/local_collect_data.jl
+++ data_collection/susceptibility/local_collect_data.jl
@@ -11,52 +11,55 @@ using Dates
 include("../../src/meanfield.jl")
 include("utilities.jl")
 
-L = 7
+L = 23
 t = 1 # hopping 
 Q = (√5 - 1) / 2
-μ = 1e-8
+μ = 0.75
 θ = π / 7
-periodic = true # periodic 
-disorder = false
+V0 = 0
+V1 = 0
 ndims = 3
-nrep = 40
-Js = [1, 4]
+disorder = false
+periodic = true
+
+Js = [0, 0.1, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 4, 5, 6]#collect(0:0.25:6)
 Ts = expspace(-3, 1, 30) # temperature 
 
-Λ = nothing
-if disorder
-    dirname = "data_random"
-else
-    dirname = "data"
-end
-if periodic
-    dirname = dirname * "_PBC"
-else
-    dirname = dirname * "_OBC"
-end
+nrep = 40
 
 # datapath
-datapath = joinpath(@__DIR__, dirname)
-mkpath(datapath)
+disorder_datapath = joinpath(@__DIR__, "data_random_$(ndims)D")
+QP_datapath = joinpath(@__DIR__, "data_QP_$(ndims)D")
+mkpath(disorder_datapath)
+mkpath(QP_datapath)
 
-for _ in 1:nrep
+Λ = 0.3
+for n in 1:nrep
     ϕx = 2π * rand()
     ϕy = 2π * rand()
     ϕz = 2π * rand()
 
     for J in Js
         for T in Ts
-            m = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=-1, V1=-1, J=J, periodic=periodic, ndims=ndims, disorder=disorder)
-
-            ## CALCULATION ## 
+            @show n
+            ## QUASIPERIODIC 
+            m = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=-1, V1=-1, J=J, periodic=periodic, ndims=ndims, disorder=false)
             χ, dχdlogT = @time uniform_susceptibility(m, T=T, calculate_dχdlogT=true, Λ=Λ)
-
-            ## SAVING ##  
             timestamp = Dates.format(now(), "yyyy-mm-dd_HH:MM:SS")
-            savepath = joinpath(datapath, "$(L)L_$(J)J" * timestamp * ".csv")
+            savepath = joinpath(disorder_datapath, "$(L)L_$(J)J" * timestamp * ".csv")
             df = DataFrame(L=[L], J=[J], Q=[Q], θ=[θ],
                 ϕx=[ϕx], ϕy=[ϕy], ϕz=[ϕz], ndims=[ndims],
-                T=[T], χ=[χ], dχdlogT=[dχdlogT], Λ=[6])
+                T=[T], χ=[χ], dχdlogT=[dχdlogT], Λ=[Λ])
+            CSV.write(savepath, df)
+
+            ## DISORDER 
+            m = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=-1, V1=-1, J=J, periodic=periodic, ndims=ndims, disorder=true)
+            χ, dχdlogT = @time uniform_susceptibility(m, T=T, calculate_dχdlogT=true, Λ=Λ)
+            timestamp = Dates.format(now(), "yyyy-mm-dd_HH:MM:SS")
+            savepath = joinpath(QP_datapath, "$(L)L_$(J)J" * timestamp * ".csv")
+            df = DataFrame(L=[L], J=[J], Q=[Q], θ=[θ],
+                ϕx=[ϕx], ϕy=[ϕy], ϕz=[ϕz], ndims=[ndims],
+                T=[T], χ=[χ], dχdlogT=[dχdlogT], Λ=[Λ])
             CSV.write(savepath, df)
         end
     end
