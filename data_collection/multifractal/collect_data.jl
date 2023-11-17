d9ea68e100355b72304962626a0b051e888882b4
nticea <nticea@stanford.edu>
2023-11-17 15:11:29 -0800

    find Jc1 and Jc2 using multifractal code

--- data_collection/multifractal/collect_data.jl
+++ data_collection/multifractal/collect_data.jl
@@ -10,41 +10,36 @@ include("../../src/model.jl")
 include("utilities.jl")
 
 ## MODEL PARAMETERS ##
+## MODEL PARAMETERS ##
 args = parse.(Float64, ARGS)
-@assert length(args) == 15
-L, t, Q, μ, θ, _, _, _, V0, V1, J, periodic, ndims, λ, E₀ = [args[n] for n in 1:length(args)]
+@assert length(args) == 16
+L, t, Q, μ, θ, ϕx, ϕy, ϕz, V0, V1, J, periodic, ndims, disorder, λ, E₀ = [args[n] for n in 1:length(args)]
 L = Int(L)
 ndims = Int(ndims)
 periodic = Bool(periodic)
+disorder = Bool(disorder)
 
 datapath = joinpath(@__DIR__, "data")
 mkpath(datapath)
 
-ϕxs = LinRange(0, π + 0.1, 3)
-ϕys = LinRange(0, π + 0.01, 3)
-ϕzs = LinRange(0, π - 0.1, 3)
-
 dfs = load_dfs()
 
-for ϕx in ϕxs
-    for ϕy in ϕys
-        for ϕz in ϕzs
-            @show L, J
+Js = LinRange(0, 5, 30)
+for J in Js
+    @show L, J
 
-            # for saving hamiltonian
-            stamp = "diagonalized_$(ndims)D_$(L)L_$(J)J_$(round(θ, digits=3))theta_$(round(Q,digits=3))Q_$(round(ϕx,digits=3))ϕx_$(round(ϕy,digits=3))ϕy_$(round(ϕz,digits=3))ϕz_$(Int(periodic))periodic.h5"
-            scratchbase = joinpath("/scratch/users/nticea", "QuasiperiodicSuperconductivity", "diagonalized_hamiltonians")
-            #scratchbase = joinpath(@__DIR__, "diagonalized_hamiltonians")
-            mkpath(scratchbase)
-            scratchpath = joinpath(scratchbase, stamp)
+    # # for saving hamiltonian
+    # stamp = "diagonalized_$(ndims)D_$(L)L_$(J)J_$(round(θ, digits=3))theta_$(round(Q,digits=3))Q_$(round(ϕx,digits=3))ϕx_$(round(ϕy,digits=3))ϕy_$(round(ϕz,digits=3))ϕz_$(Int(periodic))periodic.h5"
+    # scratchbase = joinpath("/scratch/users/nticea", "QuasiperiodicSuperconductivity", "diagonalized_hamiltonians")
+    # #scratchbase = joinpath(@__DIR__, "diagonalized_hamiltonians")
+    # mkpath(scratchbase)
+    # scratchpath = joinpath(scratchbase, stamp)
 
-            m = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=-1, V1=-1, J=J, periodic=periodic, ndims=ndims)
-            if !already_computed(dfs, m; E₀=E₀, λ=λ)
-                α₀, ipr_real, ipr_k = compute_scaling_properties(m; λ=λ, E₀=E₀, loadpath=scratchpath)
-                @show α₀, ipr_real, ipr_k
-                # save it out 
-                save_results(m, λ=λ, E₀=E₀, α₀=α₀, ipr_real=ipr_real, ipr_k=ipr_k)
-            end
-        end
+    m = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=-1, V1=-1, J=J, periodic=periodic, ndims=ndims, disorder=disorder)
+    if !already_computed(dfs, m; E₀=E₀, λ=λ)
+        α₀, ipr_real, ipr_k = compute_scaling_properties(m; λ=λ, E₀=E₀)
+        @show α₀, ipr_real, ipr_k
+        # save it out 
+        save_results(m, λ=λ, E₀=E₀, α₀=α₀, ipr_real=ipr_real, ipr_k=ipr_k)
     end
 end
--- data_collection/multifractal/utilities.jl
+++ data_collection/multifractal/utilities.jl
@@ -3,7 +3,7 @@ include("../../src/results.jl")
 
 function already_computed(df, m::ModelParams; λ, E₀)
     # extract only the parameters we are interested in 
-    df = df[(df.L.==m.L).&(df.θ.==m.θ).&(df.Q.==m.Q).&(df.ϕx.==m.ϕx).&(df.ϕy.==m.ϕy).&(df.ϕz.==m.ϕz).&(df.ndims.==m.ndims).&(df.J.==m.J).&(df.λ.==λ).&(df.E₀.==E₀), :]
+    df = df[(df.L.==m.L).&(df.θ.==m.θ).&(df.Q.==m.Q).&(df.ϕx.==m.ϕx).&(df.ϕy.==m.ϕy).&(df.ϕz.==m.ϕz).&(df.ndims.==m.ndims).&(df.disorder.==m.disorder).&(df.J.==m.J).&(df.λ.==λ).&(df.E₀.==E₀), :]
     if size(df)[1] > 0
         println("Already computed!")
         return true
@@ -18,7 +18,8 @@ function save_results(m::ModelParams; λ, E₀, α₀, ipr_real, ipr_k)
     savepath = joinpath(datapath, "$(L)L_$(J)J" * timestamp * ".csv")
     df = DataFrame(L=[m.L], J=[m.J], Q=[m.Q], θ=[m.θ],
         ϕx=[m.ϕx], ϕy=[m.ϕy], ϕz=[m.ϕz], ndims=[m.ndims],
-        λ=[λ], E₀=[E₀], α₀=[α₀], ipr_real=[ipr_real], ipr_k=[ipr_k])
+        λ=[λ], E₀=[E₀], α₀=[α₀], ipr_real=[ipr_real],
+        ipr_k=[ipr_k], disorder=[m.disorder])
     CSV.write(savepath, df)
     flush(stdout)
 end
@@ -27,7 +28,7 @@ function load_dfs()
     # read files 
     files = readdir(joinpath(@__DIR__, "data"))
     df = DataFrame(L=[], J=[], Q=[], θ=[], ϕx=[], ϕy=[], ϕz=[],
-        ndims=[], λ=[], E₀=[], α₀=[], ipr_real=[], ipr_k=[])
+        ndims=[], disorder=[], λ=[], E₀=[], α₀=[], ipr_real=[], ipr_k=[])
     for f in files
         if endswith(f, ".csv")
             try
