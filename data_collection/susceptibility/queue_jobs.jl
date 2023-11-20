e769be6ca66a9728eed79c7ce8ff388c2912eb76
nticea <nticea@stanford.edu>
2023-11-19 20:25:25 -0800

    more susceptibility

--- data_collection/susceptibility/queue_jobs.jl
+++ data_collection/susceptibility/queue_jobs.jl
@@ -41,12 +41,12 @@ for _ in 1:nrep
     end
 
     # DISORDER! 
-    for J in Js
-        for T in Ts
-            ps = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=1, ndims=ndims, disorder=1)
-            submit_job(ps, filepath, @__DIR__, job_prefix, mem=128, kwargs="$T", time="15:00")
-        end
-    end
+    # for J in Js
+    #     for T in Ts
+    #         ps = ModelParams(L=L, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=1, ndims=ndims, disorder=1)
+    #         submit_job(ps, filepath, @__DIR__, job_prefix, mem=128, kwargs="$T", time="15:00")
+    #     end
+    # end
 
     # 2D 
     # Quasiperiodic 
