73ede7be6b10f273b452232798f1808268fbb67f
nticea <nticea@stanford.edu>
2024-01-19 09:48:22 -0800

    more stiffness

--- data_collection/stiffness/queue_jobs_stiffness.jl
+++ data_collection/stiffness/queue_jobs_stiffness.jl
@@ -20,46 +20,75 @@ job_prefix = "stiffness_only"
 ϕy = 0
 ϕz = 0
 
+# QUASIPERIODIC
+# p-wave 
+Js = [2.74, 3.0, 3.24, 3.49, 3.74, 4.24, 5.24]
 for J in Js
-    # QUASIPERIODIC
-    # p-wave 
     V0 = 0
     V1 = -3
     ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=false)
     submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="48:00:00")
+end
 
-    # d-wave 
+# d-wave 
+Js = [1.75, 2, 3.24, 4.49, 4.74, 4.99, 5.24, 5.99]
+for J in Js
     V0 = 1
     V1 = -3
     ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=false)
     submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="48:00:00")
+end
 
-    # s-wave 
+# s-wave 
+Js = collect(0:0.25:6)
+push!(Js, 3.76, 4.76)
+for J in Js
     V0 = -3
     V1 = 0
     ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=false)
     submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="4:00:00")
-
-
-    # DISORDER 
-    # d-wave 
-    # V0 = 1
-    # V1 = -3
-    # ps = ModelParams(L=13, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
-    # submit_job(ps, filepath, @__DIR__, job_prefix, mem=800, time="6:00:00")
-    # ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
-    # submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="4:00:00")
-    # ps = ModelParams(L=7, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
-    # submit_job(ps, filepath, @__DIR__, job_prefix, mem=128, time="10:00")
-
-    # # s-wave 
-    # V0 = -3
-    # V1 = 0
-    # ps = ModelParams(L=13, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
-    # submit_job(ps, filepath, @__DIR__, job_prefix, mem=800, time="6:00:00")
-    # ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
-    # submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="4:00:00")
-    # ps = ModelParams(L=7, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
-    # submit_job(ps, filepath, @__DIR__, job_prefix, mem=128, time="10")
 end
+
+# for J in Js
+#     # QUASIPERIODIC
+#     # p-wave 
+#     V0 = 0
+#     V1 = -3
+#     ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=false)
+#     submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="48:00:00")
+
+#     # d-wave 
+#     V0 = 1
+#     V1 = -3
+#     ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=false)
+#     submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="48:00:00")
+
+#     # s-wave 
+#     V0 = -3
+#     V1 = 0
+#     ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=false)
+#     submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="4:00:00")
+
+
+#     # DISORDER 
+#     # d-wave 
+#     # V0 = 1
+#     # V1 = -3
+#     # ps = ModelParams(L=13, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
+#     # submit_job(ps, filepath, @__DIR__, job_prefix, mem=800, time="6:00:00")
+#     # ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
+#     # submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="4:00:00")
+#     # ps = ModelParams(L=7, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
+#     # submit_job(ps, filepath, @__DIR__, job_prefix, mem=128, time="10:00")
+
+#     # # s-wave 
+#     # V0 = -3
+#     # V1 = 0
+#     # ps = ModelParams(L=13, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
+#     # submit_job(ps, filepath, @__DIR__, job_prefix, mem=800, time="6:00:00")
+#     # ps = ModelParams(L=11, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
+#     # submit_job(ps, filepath, @__DIR__, job_prefix, mem=512, time="4:00:00")
+#     # ps = ModelParams(L=7, t=t, Q=Q, μ=μ, θ=θ, ϕx=ϕx, ϕy=ϕy, ϕz=ϕz, V0=V0, V1=V1, J=J, periodic=periodic, ndims=ndims, disorder=true)
+#     # submit_job(ps, filepath, @__DIR__, job_prefix, mem=128, time="10")
+# end
 
